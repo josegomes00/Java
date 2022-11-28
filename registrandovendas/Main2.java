50a0ec8a4f38210a6c061e9b9927b7c73e9f71c7
José Gomes <106168901+josegomes00@users.noreply.github.com>
2022-11-28 11:23:30 -0300

    Update Main2.java

--- registrandovendas/Main2.java
+++ registrandovendas/Main2.java
@@ -1,9 +1,9 @@
 package registrandovendas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main2 {
-
     public static void menu() {
         System.out.println("DIGITE [1] PARA CADASTRAR UM VENDEDOR");
         System.out.println("DIGITE [2] PARA REGISTRAR UMA VENDA");
@@ -11,109 +11,115 @@ public class Main2 {
         System.out.println("DIGITE [100] PARA FINALIZAR O PROGRAMA\n");
     }
 
-    public static void cadastrarVendedor() {
-        Scanner scanVendendor = new Scanner(System.in);
-        String nome;
-        String codigo;
-        String endereco;
-
-        System.out.println("Digite o Nome do Vendedor: ");
-        nome = scanVendendor.nextLine();
-        System.out.println();
-
-        System.out.println("Digite o Código do Vendedor: ");
-        codigo = scanVendendor.nextLine();
-        System.out.println();
-
-        System.out.println("Digite o Endereço do Vendedor: ");
-        endereco = scanVendendor.nextLine();
-        System.out.println();
-
-        Vendedor v = new Vendedor(nome, codigo, endereco);
-    }
-
-
-    public static void registrarVenda() {
-        Scanner scanVenda = new Scanner(System.in);
-        String codigo;
-        String descricao;
-        int qtdItens;
-        double desconto = 0;
-        double valorVenda;
-        double valorCusto;
-        boolean promocao = false;
-        String aux;
-
-        System.out.println("Digite o Código do Produto: ");
-        codigo = scanVenda.nextLine();
-        System.out.println();
-
-        System.out.println("Digite a Descrição do Produto: ");
-        descricao = scanVenda.nextLine();
-        System.out.println();
-
-        System.out.println("Digite o Valor de Venda do Produto: ");
-        valorVenda = scanVenda.nextDouble();
-
-        System.out.println("Digite o Valor de Custo do Produto: ");
-        valorCusto = scanVenda.nextDouble();
-
-        System.out.println("O Produto está em Promoção? [S/N]");
-        aux = scanVenda.next();
-        if (aux.equalsIgnoreCase("S")) {
-            desconto = valorVenda - ((valorVenda * 15) / 100);
-            promocao = true;
-        } else if (aux.equalsIgnoreCase("N")) {
-            promocao = false;
-        }
-        System.out.println();
-
-        System.out.println("Quantidade de Itens: ");
-        qtdItens = scanVenda.nextInt();
-
-        Produto p = new Produto(codigo, descricao, valorVenda, valorCusto, promocao);
-        Venda ven = new Venda();
-
-        //faltando setar vendedor
-        ven.setProduto(p);
-        ven.setQtdItens(qtdItens);
-        ven.setValor(valorVenda);
-        ven.setDesconto(desconto);
-
-    }
-
-    public static void exibirVenda() {
-
-    }
-
     public static void main(String[] args) {
-        int option;
+        int option = 0;
         Scanner scan = new Scanner(System.in);
+        Vendedor v = new Vendedor("","","");
+        Produto p = new Produto("", "", 0, 0, false);
+        Venda ven = new Venda();
 
         do {
             menu();
-            option = scan.nextInt();
+            try {
+                option = scan.nextInt();
+                if (option == 1){
+                    Scanner scanVendendor = new Scanner(System.in);
+                    String nome;
+                    String codigo;
+                    String endereco;
 
-            switch (option) {
-                case 100:
+                    System.out.println("Digite o Nome do Vendedor: ");
+                    nome = scanVendendor.nextLine();
+                    nome = nome.toUpperCase();
+                    System.out.println();
+
+                    System.out.println("Digite o Código do Vendedor: ");
+                    codigo = scanVendendor.nextLine();
+                    System.out.println();
+
+                    System.out.println("Digite o Endereço do Vendedor: ");
+                    endereco = scanVendendor.nextLine();
+                    endereco = endereco.toUpperCase();
+                    System.out.println();
+
+                    v.setNome(nome);
+                    v.setCodigo(codigo);
+                    v.setEndereco(endereco);
+
+                } else if (option == 2) {
+
+                    Scanner scanVenda = new Scanner(System.in);
+                    String codigo;
+                    String descricao;
+                    int qtdItens = 0;
+                    double desconto = 0;
+                    double valorVenda = 0;
+                    double valorCusto = 0;
+                    boolean promocao = false;
+                    String aux;
+
+                    System.out.println("Digite o Código do Produto: ");
+                    codigo = scanVenda.nextLine();
+                    System.out.println();
+
+                    System.out.println("Digite a Descrição do Produto: ");
+                    descricao = scanVenda.nextLine();
+                    descricao = descricao.toUpperCase();
+                    System.out.println();
+
+                    System.out.println("Digite o Valor de Venda do Produto(R$): ");
+                    valorVenda = scanVenda.nextDouble();
+
+                    System.out.println("Digite o Valor de Custo do Produto(R$): ");
+                    valorCusto = scanVenda.nextDouble();
+
+                    System.out.println("O Produto está em Promoção? [S/N]"); //CASO ESTEJA EM PROMOÇÃO, DESCONTO APLICADO DE 15%.
+                    aux = scanVenda.next();
+                    if (aux.equalsIgnoreCase("S")) {
+                        desconto = valorVenda - ((valorVenda * 15) / 100);
+                        promocao = true;
+                    } else if (aux.equalsIgnoreCase("N")) {
+                        promocao = false;
+                    }
+                    System.out.println();
+
+                    System.out.println("Quantidade de Itens: ");
+                    qtdItens = scanVenda.nextInt();
+
+                    p.setCodigo(codigo);
+                    p.setDescricao(descricao);
+                    p.setValorVenda(valorVenda);
+                    p.setValorCusto(valorCusto);
+                    p.setPromocao(promocao);
+
+                    ven.setVendedor(v);
+                    ven.setProduto(p);
+                    ven.setQtdItens(qtdItens);
+                    ven.setValor(valorVenda);
+                    ven.setDesconto(desconto);
+
+                } else if (option == 3) {
+                    ven.calcularValor();
+                    ven.imprimeVenda();
+                    System.out.println();
+
+                } else if (option == 100) {
                     System.out.println("Finalizando...");
-                    break;
 
-                case 1:
-                    cadastrarVendedor();
-                    break;
+                } else {
+                    System.out.println("Opção Inválida!!");
 
-                case 2:
-                    registrarVenda();
-                    break;
-
-                case 3:
-                    exibirVenda();
-                    break;
-
-                default:
-                    System.out.println("Opção Inválida.");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("TENTE DIGITAR NÚMEROS, AO INVÉS DE LETRAS");
+                scan.next();
+            }catch (NullPointerException e){
+                System.out.println("Possível Erro na Leitura de Dados da Venda do Produto, Repita [REGISTRAR UMA VENDA] de Forma Correta!!");
+            }catch (Exception e){
+                System.out.println("ERRO INESPERADO!!");
             }
+
+
         }while (option != 100);
     }
 }
